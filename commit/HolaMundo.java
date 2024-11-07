23f4b60b244073d901540245e422446a465945a6
Code-Ang3l <angeliglmar@iesrodeira.gal>
2024-11-07 11:39:37 +0100

    CambiosRama

---
+++ commit/HolaMundo.java
@@ -0,0 +1,6 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        // Imprimir un mensaje en la consola
+        System.out.println("Hola, mundo desde Rama 2");
+    }
+}
