74dc6b6d000384477cf92db4a7eb570bc29d5bb1
Code-Ang3l <angeliglmar@iesrodeira.gal>
2024-11-07 12:00:42 +0100

    prueba de fusion

--- commit/HolaMundo.java
+++
@@ -1,6 +0,0 @@
-public class HolaMundo {
-    public static void main(String[] args) {
-        // Imprimir un mensaje en la consola
-        System.out.println("Hola, mundo desde Rama 2");
-    }
-}
